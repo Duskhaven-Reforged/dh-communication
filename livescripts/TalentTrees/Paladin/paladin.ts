edbc4a3bb1835f7f509f27d140411815637ccd4f
One Duskhaven <nheath@duskhaven.net>
2024-07-29 00:28:23 -0500

    created paladin general for review

---
+++ livescripts/TalentTrees/Paladin/paladin.ts
@@ -0,0 +1,39 @@
+import { EmptyPrereqs, EmptySpellArray, SetChoiceNode, SetTalentNode } from "../TalentTreeLoader"
+
+export function ReloadPaladinTree() {
+    let TAB = 51
+
+    let Talent : uint32 = GetID(`Spell`, `dh-spells`, `pal-gen-loh`)
+    SetTalentNode(Talent, TAB, 2, 1, 0, false, false, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-divinesteed`)
+    SetTalentNode(Talent, TAB, 5, 1, 0, false, false, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-exorcism`)
+    SetTalentNode(Talent, TAB, 8, 1, 0, false, false, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-sotp`)
+    SetTalentNode(Talent, TAB, 4, 2, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-loh`)]: 1, [GetID(`Spell`, 'dh-spells', 'pal-gen-divinesteed')]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-pursuitofjustice`)
+    SetTalentNode(Talent, TAB, 5, 2, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-divinesteed`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-sanctified-plaets`)
+    SetTalentNode(Talent, TAB, 6, 2, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-divinesteed`)]: 1, [GetID(`Spell`, 'dh-spells', 'pal-gen-exorcism')]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-all-good-deeds`)
+    SetTalentNode(Talent, TAB, 2, 3, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-loh`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-healinglight`)
+    SetTalentNode(Talent, TAB, 3, 3, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-sotp`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-rebuke`)
+    SetTalentNode(Talent, TAB, 5, 3, 0, false, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-sotp`)]: 1, [GetID(`Spell`, 'dh-spells', 'pal-gen-pursuitofjustice')]: 1, [GetID(`Spell`, 'dh-spells', 'pal-gen-sanctified-plaets')]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-crusade`)
+    SetTalentNode(Talent, TAB, 7, 3, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-sanctified-plaets`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-evil-be-judged`)
+    SetTalentNode(Talent, TAB, 8, 3, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-exorcism`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-purify`)
+    SetTalentNode(Talent, TAB, 2, 4, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-all-good-deeds`)]: 1, [GetID(`Spell`, `dh-spells`, `pal-gen-healinglight`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-replenishing-strikes`)
+    SetTalentNode(Talent, TAB, 4, 4, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-rebuke`)]: 1, [GetID(`Spell`, `dh-spells`, `pal-gen-healinglight`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-seals-of-the-just`)
+    SetTalentNode(Talent, TAB, 8, 4, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-crusade`)]: 1, [GetID(`Spell`, `dh-spells`, `pal-gen-evil-be-judged`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-no-mercy-for-the-wicked`)
+    SetTalentNode(Talent, TAB, 7, 4, 0, true, false, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pal-gen-rebuke`)]: 1, [GetID(`Spell`, `dh-spells`, `pal-gen-crusade`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pal-gen-punishment`)
+    SetChoiceNode(Talent, TAB, 5, 4, 0, true, CreateArray<uint32>([Talent, GetID(`Spell`, `dh-spells`, `pal-gen-example`)]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, 'dh-spells', 'pal-gen-rebuke')]: 1}), EmptySpellArray)
+
+}
