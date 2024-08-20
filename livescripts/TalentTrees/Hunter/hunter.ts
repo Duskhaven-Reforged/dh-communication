1ba0d7e265d411104492ff3bb5122bb5599a9a9b
hater <hater.rip@proton.me>
2024-08-19 21:52:24 -0400

    updates

--- livescripts/TalentTrees/Hunter/hunter.ts
+++ livescripts/TalentTrees/Hunter/hunter.ts
@@ -3,5 +3,8 @@ import { EmptyPrereqs, EmptySpellArray, SetChoiceNode, SetTalentNode } from "../
 export function ReloadHunterTree() {
     let TAB = 53
 
-    
+    QueryWorld(`Delete from forge_talents where talentTabId = ${TAB}`)
+    QueryWorld(`Delete from forge_talent_prereq where talentTabId = ${TAB}`)
+    QueryWorld(`Delete from forge_talent_ranks where talentTabId = ${TAB}`)
+    QueryWorld(`Delete from forge_talent_unlearn where talentTabId = ${TAB}`)
 }
