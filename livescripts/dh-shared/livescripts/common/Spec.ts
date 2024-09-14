f7b634ddc9700821fc4f9f3d7cf466ed2fb76d4b
hater <hater.rip@proton.me>
2024-09-14 15:20:00 -0400

    updates

--- livescripts/dh-shared/livescripts/common/Spec.ts
+++
@@ -1,5 +0,0 @@
-export enum SpecTabs {
-    ARMS = 1, FURY = 2, PWAR = 3, HPAL = 4, PPAL = 5, RPAL = 6, BMHU = 7, MMHU = 8, SVHU = 9, ASSR = 10, CORR = 11, SUBR = 12,
-    HPRI = 13, DISC = 14, SPRI  = 15, BDK = 16, FDK = 17, UDK = 18, ELEM = 19, ENHA = 20, RSHA = 21, ARCA = 22, FIRE = 23,
-    FROS = 24, ALCK = 25, DEMO = 26, DEST = 27, BALD = 28, FERA = 29, RDRU = 30, GUAR = 31, WARD = 32
-}
