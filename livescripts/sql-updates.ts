03c0438fd09356e4cb773647ee5e60fdd4f805d0
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2024-09-27 20:33:03 +0200

    Allfi 25-50 tree layout

--- livescripts/sql-updates.ts
+++ livescripts/sql-updates.ts
@@ -2,14 +2,12 @@
 // But also I'm a lazy ass, instead of making SQL files in the core (then updating sumbodule head) some stuff lands here :P
 
 export function UpdateSQLTables() {
-     QueryWorld(`Delete from spell_learn_spell where entry = 58984`)
+    QueryWorld(`Delete from spell_learn_spell where entry = 58984`)
 
     QueryWorld(`Delete from spell_pet_auras where pet in (416, 417, 1860, 1863, 17252)`)
 
     //Aleist3r: Another dirty hack since .Proc.Cooldown doesn't exist
-    let Spell : uint32 = GetID(`Spell`, 'dh-spells', 'warl-aff-eradication')
-    QueryWorld(`update spell_proc set Cooldown = 20000 where SpellID = ${Spell}`)
-    Spell = GetID(`Spell`, 'dh-spells', 'warl-des-inferno')
+    let Spell = GetID(`Spell`, 'dh-spells', 'warl-des-inferno')
     QueryWorld(`update spell_proc set Cooldown = 6000 where SpellID = ${Spell}`)
     Spell = GetID(`Spell`, 'dh-spells', 'mag-gen-magicansflourish')
     QueryWorld(`insert ignore into spell_proc (SpellId, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, ProcFlags, SpellTypeMask, SpellPhaseMask, HitMask, AttributesMask, DisableEffectsMask, ProcsPerMinute, Chance, Cooldown, Charges, __tswow_tag) values
