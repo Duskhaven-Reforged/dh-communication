80f63c1260963bc7ae1d91d7a72b461b9fcb4f99
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2024-09-29 04:42:23 +0200

    Destro 25-50 tree layout

--- livescripts/TalentTrees/Warlock/Demonology.ts
+++ livescripts/TalentTrees/Warlock/Demonology.ts
@@ -20,24 +20,24 @@ export function ReloadDemoLockTree() {
     QueryWorld(`Delete from forge_talent_unlearn where talentTabId = ${TAB}`)
 
     let Talent : uint32 = GetID(`Spell`, 'dh-spells', 'warl-dem-grimoirefelguard')
-    SetTalentNode(Talent, TAB, 5, 1, 0, false, 2**(TAB-1), CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+    SetTalentNode(Talent, TAB, 6, 1, 0, false, 2**(TAB-1), CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
 
     Talent = GetID(`Spell`, `dh-spells`, `warl-dem-demonbolt`)
-    SetTalentNode(Talent, TAB, 5, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoirefelguard`)]: 1}), EmptySpellArray, EmptySpellArray)
+    SetTalentNode(Talent, TAB, 6, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoirefelguard`)]: 1}), EmptySpellArray, EmptySpellArray)
 
     Talent = GetID(`Spell`, `dh-spells`, `warl-dem-grimoirefelhunter`)
-    SetChoiceNode(Talent, TAB, 4, 3, 0, false, CreateArray<uint32>([Talent, GetID(`Spell`, `dh-spells`, `warl-dem-grimoirevoidwalker`)]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-demonbolt`)]: 1}), EmptySpellArray)
+    SetChoiceNode(Talent, TAB, 5, 3, 0, false, CreateArray<uint32>([Talent, GetID(`Spell`, `dh-spells`, `warl-dem-grimoirevoidwalker`)]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-demonbolt`)]: 1}), EmptySpellArray)
     Talent = GetID(`Spell`, `dh-spells`, `warl-dem-sufferandperish`)
-    SetTalentNode(Talent, TAB, 5, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-demonbolt`)]: 1}), EmptySpellArray, EmptySpellArray)
+    SetTalentNode(Talent, TAB, 6, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-demonbolt`)]: 1}), EmptySpellArray, EmptySpellArray)
     Talent = GetID(`Spell`, `dh-spells`, `warl-dem-grimoiresuccubus`)
-    SetChoiceNode(Talent, TAB, 6, 3, 0, false, CreateArray<uint32>([Talent, GetID(`Spell`, `dh-spells`, `warl-dem-grimoireimp`)]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-demonbolt`)]: 1}), EmptySpellArray)
+    SetChoiceNode(Talent, TAB, 7, 3, 0, false, CreateArray<uint32>([Talent, GetID(`Spell`, `dh-spells`, `warl-dem-grimoireimp`)]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-demonbolt`)]: 1}), EmptySpellArray)
 
     Talent = GetID(`Spell`, `dh-spells`, `warl-dem-drawntocorruption`)
-    SetTalentNode(Talent, TAB, 3, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoirefelhunter`)]: 1}), EmptySpellArray, EmptySpellArray)
-    Talent = GetID(`Spell`, `dh-spells`, `warl-dem-shadowyassistance`)
     SetTalentNode(Talent, TAB, 4, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoirefelhunter`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `warl-dem-shadowyassistance`)
+    SetTalentNode(Talent, TAB, 5, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoirefelhunter`)]: 1}), EmptySpellArray, EmptySpellArray)
     Talent = GetID(`Spell`, `dh-spells`, `warl-dem-burningsynergy`)
-    SetTalentNode(Talent, TAB, 6, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoiresuccubus`)]: 1}), EmptySpellArray, EmptySpellArray)
-    Talent = GetID(`Spell`, `dh-spells`, `warl-dem-mothtoflame`)
     SetTalentNode(Talent, TAB, 7, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoiresuccubus`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `warl-dem-mothtoflame`)
+    SetTalentNode(Talent, TAB, 8, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `warl-dem-grimoiresuccubus`)]: 1}), EmptySpellArray, EmptySpellArray)
 }
--- livescripts/sql-updates.ts
+++ livescripts/sql-updates.ts
@@ -7,9 +7,7 @@ export function UpdateSQLTables() {
     QueryWorld(`Delete from spell_pet_auras where pet in (416, 417, 1860, 1863, 17252)`)
 
     //Aleist3r: Another dirty hack since .Proc.Cooldown doesn't exist
-    let Spell = GetID(`Spell`, 'dh-spells', 'warl-des-inferno')
-    QueryWorld(`update spell_proc set Cooldown = 6000 where SpellID = ${Spell}`)
-    Spell = GetID(`Spell`, 'dh-spells', 'mag-gen-magicansflourish')
+    let Spell = GetID(`Spell`, 'dh-spells', 'mag-gen-magicansflourish')
     QueryWorld(`insert ignore into spell_proc (SpellId, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, ProcFlags, SpellTypeMask, SpellPhaseMask, HitMask, AttributesMask, DisableEffectsMask, ProcsPerMinute, Chance, Cooldown, Charges, __tswow_tag) values
     (${Spell}, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1000, 0, 0);)`)
 }
