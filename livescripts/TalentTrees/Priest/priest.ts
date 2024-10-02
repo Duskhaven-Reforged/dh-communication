3f657cefc0bfe07305facc12851c9f99e8073221
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2024-10-03 00:32:14 +0200

    Priest general tree layout 10-25

--- livescripts/TalentTrees/Priest/priest.ts
+++ livescripts/TalentTrees/Priest/priest.ts
@@ -2,54 +2,60 @@ import { EmptyPrereqs, EmptySpellArray, SetChoiceNode, SetTalentNode, SpecTabs }
 
 export function ReloadPriestTree() {
     let TAB: uint32 = 55
-    let ALLMASK = (1 << SpecTabs.HPRI - 1) | 1 << (SpecTabs.DISC - 1) | 1 << (SpecTabs.SPRI - 1) | (1 << SpecTabs.INQU - 1)
+    let ALLMASK = (2**(SpecTabs.HPRI - 1)) + (2**(SpecTabs.DISC - 1)) + (2**(SpecTabs.SPRI - 1)) + (2**(SpecTabs.INQU - 1))
+    let HOLYDISC = (2**(SpecTabs.HPRI - 1)) + (2**(SpecTabs.DISC - 1))
+    let HOLYINQ = (2**(SpecTabs.HPRI - 1)) + (2**(SpecTabs.INQU - 1))
+    let SHADISC = (2**(SpecTabs.DISC - 1)) + (2**(SpecTabs.SPRI - 1))
 
     QueryWorld(`Delete from forge_talents where talentTabId = ${TAB}`)
     QueryWorld(`Delete from forge_talent_prereq where talentTabId = ${TAB}`)
     QueryWorld(`Delete from forge_talent_ranks where talentTabId = ${TAB}`)
     QueryWorld(`Delete from forge_talent_unlearn where talentTabId = ${TAB}`)
 
-    //let Talent : uint32 = 41635
-    //SetTalentNode(Talent, TAB, 3, 1, 0, false, 1 << (SpecTabs.DISC - 1) | 1 << (SpecTabs.HPRI - 1), CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-purify`)
-    //SetTalentNode(Talent, TAB, 4, 1, 0, false, ALLMASK, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-nova`)
-    //SetTalentNode(Talent, TAB, 6, 1, 0, false, 1 << (SpecTabs.HPRI - 1), CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
-    //Talent = 32379
-    //SetTalentNode(Talent, TAB, 7, 1, 0, false, 1 << (SpecTabs.DISC - 1) | 1 << (SpecTabs.SPRI - 1), CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-focusmend`)
-    //SetTalentNode(Talent, TAB, 2, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[41635]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-shackle`)
-    //SetTalentNode(Talent, TAB, 3, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[41635]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-purify`)]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-`)      Soul Tap - not in ts and no spell ID in Dochaven
-    //SetTalentNode(Talent, TAB, 4, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-purify`)]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-`)  Leap of Faith - not in ts and no spell ID in Dochaven
-    //SetTalentNode(Talent, TAB, 5, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-purify`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-nova`)]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-rhap`)
-    //SetTalentNode(Talent, TAB, 6, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-nova`)]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = 34433
-    //SetTalentNode(Talent, TAB, 7, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-nova`)]: 1, [32379]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-`)  Death and Madness - not in ts and no spell ID in Dochaven
-    //SetTalentNode(Talent, TAB, 8, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[32379]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-`)   Light Resonance - not in ts and no spell ID in Dochaven
-    //SetTalentNode(Talent, TAB, 2, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-focusmend`)]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-`)   Inquisitor's Cell - not in ts and no spell ID in Dochaven
-    //SetTalentNode(Talent, TAB, 3, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-shackle`)]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-silence`)
-    //SetTalentNode(Talent, TAB, 4, 3, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-shackle`)]: 1/*, [GetID(`Spell`, `dh-spells`, `pri-gen-shackle`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-shackle`)]: 1*/}), EmptySpellArray, EmptySpellArray)
-    //Talent = 1140032
-    //SetTalentNode(Talent, TAB, 5, 3, 0, true, 0, CreateArray<uint32>([Talent]), EmptyPrereqs/*CreateDictionary<uint32, uint8>({[Leap of Faith ID]: 1})*/, EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-flashlight`)
-    //SetTalentNode(Talent, TAB, 6, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-rhap`)]: 1/*, [Leap of Faith ID]: 1*/ }), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-`)   Choice node Empowered Aberration / Agressive Aberration - not in ts and no spell ID in Dochaven
-    //SetChoiceNode(Talent, TAB, 7, 3, 0, true, CreateArray<uint32>([Talent, GetID(`Spell`, 'dh-spells', '')]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, '')]: 1}), EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-`)   Tithe - - not in ts and no spell ID in Dochaven
-    //SetTalentNode(Talent, TAB, 8, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[]]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-efficientholy`)
-    //SetTalentNode(Talent, TAB, 3, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-silence`)]: 1/*, [Light Resonance ID]: 1, [Inquisitor's Cell ID]: 1*/}), EmptySpellArray, EmptySpellArray)
-    //Talent = 32375
-    //SetTalentNode(Talent, TAB, 5, 4, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-silence`)]: 1, [1140032]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-flashlight`)]: 1}), EmptySpellArray, EmptySpellArray)
-    //Talent = GetID(`Spell`, `dh-spells`, `pri-gen-efficientshadow`)
-    //SetTalentNode(Talent, TAB, 7, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-flashlight`)]: 1/*, [Choice node Empowered Aberration / Agressive Aberration ID)]: 1, [Tithe Spell ID]: 1*/}), EmptySpellArray, EmptySpellArray)
+    let Talent : uint32 = GetID(`Spell`, 'dh-spells', `pri-gen-prayerofmending`)
+    SetTalentNode(Talent, TAB, 4, 1, 0, false, HOLYDISC, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-purify`)
+    SetTalentNode(Talent, TAB, 5, 1, 0, false, ALLMASK, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-holynova`)
+    SetTalentNode(Talent, TAB, 7, 1, 0, false, HOLYINQ, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-shadowworddeath`)
+    SetTalentNode(Talent, TAB, 8, 1, 0, false, SHADISC, CreateArray<uint32>([Talent]), EmptyPrereqs, EmptySpellArray, EmptySpellArray)
+
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-focusedmending`)
+    SetTalentNode(Talent, TAB, 3, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-prayerofmending`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-shackleevil`)
+    SetTalentNode(Talent, TAB, 4, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-prayerofmending`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-purify`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-soultap`)
+    SetTalentNode(Talent, TAB, 5, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-purify`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-leapoffaithdummy`)
+    SetTalentNode(Talent, TAB, 6, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-purify`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-holynova`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-rhapsody`)
+    SetTalentNode(Talent, TAB, 7, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-holynova`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-twilightfiend`)
+    SetTalentNode(Talent, TAB, 8, 2, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-holynova`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-shadowworddeath`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-deathandmadness`)
+    SetTalentNode(Talent, TAB, 9, 2, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-shadowworddeath`)]: 1}), EmptySpellArray, EmptySpellArray)
+
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-lightresonance`)
+    SetTalentNode(Talent, TAB, 3, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-focusedmending`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-inquisitorscell`)
+    SetTalentNode(Talent, TAB, 4, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-shackleevil`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-silence`)
+    SetTalentNode(Talent, TAB, 5, 3, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-shackleevil`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-soultap`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-leapoffaithdummy`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-movewithgrace`)
+    SetTalentNode(Talent, TAB, 6, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-leapoffaithdummy`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-resplendentlight`)
+    SetTalentNode(Talent, TAB, 7, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-rhapsody`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, `dh-spells`, `pri-gen-empoweredaberration`)
+    SetChoiceNode(Talent, TAB, 8, 3, 0, true, CreateArray<uint32>([Talent, GetID(`Spell`, `dh-spells`, `pri-gen-agressiveaberration`)]), CreateDictionary<uint32, uint8>({[GetID('Spell', 'dh-spells', 'pri-gen-twilightfiend')]: 1}), EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-tithe`)
+    SetTalentNode(Talent, TAB, 9, 3, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-deathandmadness`)]: 1}), EmptySpellArray, EmptySpellArray)
+    
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-efficientholycasting`)
+    SetTalentNode(Talent, TAB, 4, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-lightresonance`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-inquisitorscell`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-silence`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-massdispel`)
+    SetTalentNode(Talent, TAB, 6, 4, 0, false, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-silence`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-movewithgrace`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-resplendentlight`)]: 1}), EmptySpellArray, EmptySpellArray)
+    Talent = GetID(`Spell`, 'dh-spells', `pri-gen-darkdevotion`)
+    SetTalentNode(Talent, TAB, 8, 4, 0, true, 0, CreateArray<uint32>([Talent]), CreateDictionary<uint32, uint8>({[GetID(`Spell`, `dh-spells`, `pri-gen-resplendentlight`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-empoweredaberration`)]: 1, [GetID(`Spell`, `dh-spells`, `pri-gen-tithe`)]: 1}), EmptySpellArray, EmptySpellArray)
 
 }
